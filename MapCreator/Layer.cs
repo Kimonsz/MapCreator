22f7f72fd7dae77a080e8328f8a9aef4b94f442b
Kimonsz <109304632+Kimonsz@users.noreply.github.com>
2022-07-14 21:09:19 +0200

    Base

---
+++ MapCreator/Layer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+
+namespace MapCreator
+{
+    class Layer
+    {
+        public int zValue; //information about Z- axis of layer
+
+        public Layer(int zVal)
+        {
+            this.zValue = zVal;
+
+        }
+    }
+}
---
+++ MapCreator/Map.cs
@@ -0,0 +1,27 @@
+﻿using System.Drawing;
+
+namespace MapCreator
+{
+    class Map
+    {
+        private int xValue;     //number of squares in x axis
+        private int yValue;     //number of squares in y axis
+        private int xPxValue;   //number of pixels in x axis
+        private int yPxValue;   //number of Pixels in y axis
+        private int squareSize;
+        private Bitmap mainBitmap;
+        private int gridSize;
+
+        public Map(int xVal,int yVal,int gridSize,int sqrSize)
+        {
+            xValue = xVal;
+            yValue = yVal;
+            squareSize = sqrSize;
+            this.gridSize = gridSize;
+            xPxValue = xValue * squareSize + gridSize * xValue - 1;
+            yPxValue = yValue * squareSize + gridSize * yValue - 1;
+
+            mainBitmap = new Bitmap(xPxValue,yPxValue);
+        }
+    }
+}
